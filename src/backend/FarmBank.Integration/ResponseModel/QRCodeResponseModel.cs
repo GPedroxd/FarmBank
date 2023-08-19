d2fd1d92299e42d166473a6f85261dfa4367f31e
Vinicius Santos <vinicius.santos@radixeng.com.br>
2023-08-19 03:25:35 -0300

    Fix mercado pago models

--- src/backend/FarmBank.Integration/ResponseModel/QRCodeResponseModel.cs
+++ src/backend/FarmBank.Integration/ResponseModel/QRCodeResponseModel.cs
@@ -5,10 +5,25 @@ namespace FarmBank.Integration.ResponseModel;
 public struct QRCodeResponseModel
 {
     [JsonPropertyName("id")]
-    public string TransactionId { get; set; }
+    public long TransactionId { get; set; }
     [JsonPropertyName("date_created")]
     public DateTime CreatedAt { get; set; }
     [JsonPropertyName("date_of_expiration")]
     public DateTime ExpirationDate { get; set; }
-    public QRCodeDataResponseModel Data { get; set; }
+    [JsonPropertyName("point_of_interaction")]
+    public PointOfInteractionResponseModel PointOfInteraction { get; set; }
+}
+
+public struct PointOfInteractionResponseModel {
+    [JsonPropertyName("transaction_data")]
+    public TransactionDataResponseModel TransactionData { get; set; }
+}
+
+public struct TransactionDataResponseModel
+{
+    [JsonPropertyName("qr_code")]
+    public string QRCodeCopyPaste { get; set; }
+    [JsonPropertyName("qr_code_base64")]
+    public string QRCodeBase64 { get; set; }
+
 }
