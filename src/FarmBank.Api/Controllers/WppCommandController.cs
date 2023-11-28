17e59d559eb4f7ba6331a8ddc929f78b0eb16d0a
GPedroXd <contato.tec.pedro@gmail.com>
2023-11-28 00:59:32 -0300

    creating wppcommand endpoint and mapping input model

---
+++ src/FarmBank.Api/Controllers/WppCommandController.cs
@@ -0,0 +1,27 @@
+using FarmBank.Application.Commands.WppCommand;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmBank.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WppCommandController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public WppCommandController(IMediator mediator)
+        => _mediator = mediator;
+    
+    /// <summary>
+    /// endpoint to receive wpp callback message
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> WppCallback(WppCommand wppCommand)
+    {
+        await Task.CompletedTask;
+
+        return Ok();
+    }
+}
