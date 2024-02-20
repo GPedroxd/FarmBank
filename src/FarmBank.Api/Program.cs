5f68b48a7617de92414405113fc2418fac44c224
Vinicius Santos <vinicius.santos@radixeng.com>
2024-02-20 12:38:42 -0300

    Set root url to default swagger route

--- src/FarmBank.Api/Program.cs
+++ src/FarmBank.Api/Program.cs
@@ -22,7 +22,9 @@ builder.Services.AddSwaggerGen();
 BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
 
 builder.Services.AddBackgroundService();
-builder.Services.AddScoped(_ => new MongoContext(builder.Configuration["MongoDbConnectionString"], "FarmBank"));
+builder.Services.AddScoped(
+    _ => new MongoContext(builder.Configuration["MongoDbConnectionString"], "FarmBank")
+);
 builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddMediatR(conf => conf.RegisterServicesFromAssemblyContaining<NewPixCommand>());
@@ -30,44 +32,55 @@ builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<IWppService, WppService>();
 
 var wppConfig = new GeneralConfigs(
-    builder.Configuration["WppGroupId"], 
+    builder.Configuration["WppGroupId"],
     builder.Configuration["WppBotInstanceKey"],
     builder.Configuration["FrontEndUrl"]
 );
 builder.Services.AddSingleton(wppConfig);
 
-builder.Services.AddRefitClient<IMercadoPagoApi>(new()
-{
-    AuthorizationHeaderValueGetter = (msg, ct) => Task.FromResult(builder.Configuration["MercadoPagoApiToken"]!),
-}).ConfigureHttpClient(c =>
-{
-    c.BaseAddress = new Uri("https://api.mercadopago.com");
-    c.Timeout = TimeSpan.FromMinutes(3);
-});
+builder
+    .Services.AddRefitClient<IMercadoPagoApi>(
+        new()
+        {
+            AuthorizationHeaderValueGetter = (msg, ct) =>
+                Task.FromResult(builder.Configuration["MercadoPagoApiToken"]!),
+        }
+    )
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri("https://api.mercadopago.com");
+        c.Timeout = TimeSpan.FromMinutes(3);
+    });
 
-var retryPolicy = HttpPolicyExtensions.
-    HandleTransientHttpError().
-    OrResult( msg => !msg.IsSuccessStatusCode).
-    WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+var retryPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => !msg.IsSuccessStatusCode)
+    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
-builder.Services.AddRefitClient<IWppApi>()
-.ConfigureHttpClient(c =>
-{
-    c.BaseAddress = new Uri(builder.Configuration["WppApiUrl"] ?? "localhost:3333");
-    c.Timeout = TimeSpan.FromMinutes(3);
-})
-.AddPolicyHandler(retryPolicy);
+builder
+    .Services.AddRefitClient<IWppApi>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["WppApiUrl"] ?? "localhost:3333");
+        c.Timeout = TimeSpan.FromMinutes(3);
+    })
+    .AddPolicyHandler(retryPolicy);
 
 builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FarmBank.Api v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
-app.UseCors(c => {
+app.UseCors(c =>
+{
     c.AllowAnyHeader();
     c.WithMethods("POST");
     c.AllowAnyOrigin();
